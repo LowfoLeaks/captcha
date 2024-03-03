59e7b580dd73c3eab9d2a3bebf519877bc0efde5
LowfoLeaks <161240736+LowfoLeaks@users.noreply.github.com>
2024-03-03 20:12:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -105,6 +105,7 @@ async function check_base(conteudo, num) {
   return false
 }
 
+/*
 app.use(async (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Headers", "*")
@@ -127,6 +128,26 @@ app.use(async (req, res, next) => {
   }
   next()
 })
+*/
+
+app.use((req, res, next) => {
+  // try it out at http://localhost:3000/?query=hello+world and check the console
+  const start = Date.now();
+  res.on("finish", () => {
+    const responseTime = Date.now() - start;
+    const contentLength = res.get("Content-Length");
+    console.log({
+      method: req.method,
+      url: req.originalUrl,
+      query: req.query,
+      responseTime: `${responseTime} ms`,
+      contentLength: `${contentLength} bytes`,
+      status: res.statusCode,
+    });
+  });
+  // the next function is a callback that tells express to move on to the next middleware or route handler
+  next();
+});
 
 app.get('/verification', express.static(path.join(__dirname, 'util', 'verification')), async function (req, res) {
   res.status(200).sendFile(path.join(__dirname, 'util', 'verification', 'index.html'))
